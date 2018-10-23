68ac2ee9c7549119737f1ac1622148c9638456b6
Ammar Arjomand <persiancookbook@gmail.com>
2018-10-23 11:27:47 +0100

    migrations fixed - single table only; added Deal class to admin UI

--- noobs/weCar/models.py
+++ noobs/weCar/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+import datetime
+
 
 # Create your models here.
 class Deal(models.Model):
     title = models.CharField(max_length=250)
-    details = models.TextField
-    RRP = models.IntegerField
-    price = models.IntegerField
-    tippingPoint = models.IntegerField
-    expiry = models.DateTimeField
-    pic = models.ImageField
+    details = models.TextField(default='Insert deal description here')
+    RRP = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
+    tippingPoint = models.IntegerField(default=0)
+    expiry = models.DateTimeField(default=datetime.datetime(2020,1,1))
+    pic = models.CharField(max_length=500, default='http://trotters-van-hire.co.uk/wp-content/uploads/2016/11/DelBoyVan.jpg')
+
+    def __str__(self):
+        return self.title
