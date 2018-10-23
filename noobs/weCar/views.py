f8b05a39febf87953b56f9ed6e64921fd3b4ce4f
Mohammad Seyedarabi <m.seyedarabi@gmail.com>
2018-10-23 14:31:12 +0100

    changed return request for views.py to render shortcut

--- noobs/weCar/views.py
+++ noobs/weCar/views.py
@@ -2,28 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Deal
 from django.template import loader
-
+from django.shortcuts import render
 import json
 
 
 # Create your views here.
 
-
-
-
-
 def index(request):
     all_deals = Deal.objects.all()
-    template = loader.get_template('weCar/index.html')
-    context = {
-         'all_deals' : all_deals,
-    }
-
-    # html = ''
-    # for deal in all_deals:
-    #     url = '/wecar/' + str(deal.id) + '/'
-    #     html += '<a href="' + url +  '">' + deal.title + '</a><br>'
-    return HttpResponse(template.render(context, request))
+    context = {'all_deals' : all_deals}
+    return render(request, 'weCar/index.html', context)
 
 def detail(request, deal_id):
     html = "<h2>Details for deals id: " + str(deal_id) + "</h2>"
