f54fa89b1291e91225b2daff766de46b1163e877
Ammar Arjomand <persiancookbook@gmail.com>
2018-10-23 16:26:10 +0100

    hard coded URLs removed::detail.html template created

--- noobs/weCar/views.py
+++ noobs/weCar/views.py
@@ -1,22 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Deal
-from django.template import loader
-from django.shortcuts import render
-import json
+from django.http import Http404
 
 
 # Create your views here.
 
-
-
-
-
 def index(request):
     all_deals = Deal.objects.all()
-    context = {'all_deals' : all_deals}
-    return render(request, 'weCar/index.html', context)
+    return render(request, 'weCar/index.html', {'all_deals' : all_deals})
 
 def detail(request, deal_id):
-    html = "<h2>Details for deals id: " + str(deal_id) + "</h2>"
-    return HttpResponse(html)
+    try:
+        deal = Deal.objects.get(pk=deal_id)
+    except Deal.DoesNotExist:
+        raise Http404("Deal cannot be found")
+    return render(request, 'weCar/detail.html', {'deal': deal})
