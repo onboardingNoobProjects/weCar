a86a080f31e8aa9d4706a6e9f3b3efb203099058
Ammar Arjomand <persiancookbook@gmail.com>
2018-10-25 17:04:58 +0100

    user auth working, user

--- noobs/weCar/urls.py
+++ noobs/weCar/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
+from django.contrib.auth import logout
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
     # /wecar/
@@ -17,7 +20,13 @@ urlpatterns = [
     # /wecar/deal/<pk>/delete
     url(r'^deal/(?P<pk>[0-9]+)/delete/$', views.DealDelete.as_view(), name='deal-delete'),
 
-    # # /wecar/register
-    # url(r'^register/$', views.UserForm.as_view(), name='register'),
+    # /wecar/register
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+
+    # /wecar/logout
+    # url(r'^logout/$', views.logout_view , name='logout')
+    # url(r'^logout/$', views.Logout.as_view(), name='logout'),
+    url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout', kwargs={ 'next_page' : 'index', }),
 
 ]
