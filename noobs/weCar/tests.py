68f26a9901a2f194c195f5b27c1799876299f2d4
Ammar Arjomand <persiancookbook@gmail.com>
2018-10-30 11:30:43 +0000

    url redirects to homepage // fixed vannishing edit button

--- noobs/weCar/tests.py
+++ noobs/weCar/tests.py
@@ -1,46 +1,11 @@
 from django.test import TestCase
-
-# Create your tests here.
-# import unittest
-# from selenium import webdriver
-# import requests
-#
-# class SeleniumCBT(unittest.TestCase):
-#     def setUp(self):
-#
-#         self.username = "user@email.com"
-#         self.authkey  = "12345"
-#
-#         self.api_session = requests.Session()
-#         self.api_session.auth = (self.username,self.authkey)
-#         self.test_result = None
-#
-#         caps = {}
-#         caps['browserName'] = 'Chrome'
-#         caps['version'] = '60x64'
-#         caps['platform'] = 'Windows 10'
-#         caps['screenResolution'] = '1366x768'
-#
-#         self.driver = webdriver.Remote(
-#             desired_capabilities=caps,
-#             command_executor="http://%s:%s@hub.crossbrowsertesting.com:80/wd/hub"%(self.username,self.authkey)
-#         )
-#
-#         self.driver.implicitly_wait(20)
-#
-#     def test_CBT(self):
-#             self.driver.get('http://crossbrowsertesting.github.io/selenium_example_page.html')
-#             self.assertEqual("Selenium Test Example Page", self.driver.title)
-#             self.test_result = 'pass'
-#             self.driver.quit()
-#
-#
-# if __name__ == '__main__':
-#     unittest.main()
-
-
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 browser = webdriver.Chrome()
-browser.get('https://google.com/')
+
+browser.get('localhost:8000/wecar/')
+assert 'BMW' in browser.page_source
+
+browser.quit()
