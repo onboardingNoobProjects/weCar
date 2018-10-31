eb03a48952142592b5ba9d9e022e22de8846e106
Mohammad Seyedarabi <m.seyedarabi@gmail.com>
2018-10-31 16:56:16 +0000

    added more unittest to test_add.py and added classes for toolbar to data.py

--- noobs/weCar/tests/data.py
+++ noobs/weCar/tests/data.py
@@ -22,10 +22,26 @@ class Details:
     descriptionElement = '/html/body/div'
 
 class AddDeals_vars:
+
     addDeal_url = 'localhost:8000/wecar/add'
     addDeal_labels = ['Title:','Details:','RRP:','Price:','TippingPoint:','Expiry:','Pic:']
+    addDeal_title = 'BMW i3'
+    addDeal_details = 'Great Green car helping the enviorment'
+    addDeal_rrp = '£35000'
+    addDeal_price = '£40000'
+    addDeal_tippingPoint = '100'
+    addDeal_expiry = '2020-01-01 00:10:00'
+    addDeal_pic = 'http://trotters-van-hire.co.uk/wp-content/uploads/2016/11/DelBoyVan.jpg'
 
 
+    addDeal_title_xpath = '//*[@id="id_title"]'
+    addDeal_details_xpath = '//*[@id="id_details"]'
+    addDeal_rrp_xpath = '//*[@id="id_RRP"]'
+    addDeal_price_xpath = '//*[@id="id_price"]'
+    addDeal_tippingPoint_xpath = '//*[@id="id_tippingPoint"]'
+    addDeal_expiry_xpath = '//*[@id="id_expiry"]'
+    addDeal_pic_xpath = '//*[@id="id_pic"]'
+    addDeal_btSubmit_xpath = '/html/body/form/div[8]/div/button'
 class ToolBar:
         carName = 'tesla'
         btDeals = '//*[@id="topNavBar"]/ul[1]/li[1]/a'
