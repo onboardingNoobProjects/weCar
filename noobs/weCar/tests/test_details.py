25f9d042f9bdc7af52e5a3dd8d243f71e886eec6
Ammar Arjomand <persiancookbook@gmail.com>
2018-10-31 13:56:32 +0000

    more tests and reforcted all tests ond details

--- noobs/weCar/tests/test_details.py
+++ noobs/weCar/tests/test_details.py
@@ -2,12 +2,13 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import helper
+import data
 
 class Details(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
-        self.browser.get('localhost:8000/wecar/1')
+        self.browser.get(data.Details.detailsURL)
         helper.login(self)
 
     def tearDown(self):
@@ -17,27 +18,35 @@ class Details(unittest.TestCase):
         self.assertIn('WeCar', self.browser.title)
 
     def testImageExists(self):
-        elem = self.browser.find_element_by_xpath('/html/body/img')
+        elem = self.browser.find_element_by_xpath(data.Details.imageElement)
         self.assertTrue(elem)
 
     def testH1Exists(self):
-        elem = self.browser.find_element_by_xpath('/html/body/h1')
+        elem = self.browser.find_element_by_xpath(data.Details.titleElement)
         self.assertTrue(elem)
 
+    def testPcExists(self):
+        elem = self.browser.find_element_by_xpath(data.Details.titleElement)
+        self.assertIn('%', elem.text)
+
     def testH3Exists(self):
-        elem = self.browser.find_element_by_xpath('/html/body/h3')
+        elem = self.browser.find_element_by_xpath(data.Details.priceElement)
         self.assertTrue(elem)
 
     def testTippingPointExists(self):
-        self.assertIn('Tipping point:', self.browser.page_source)
+        self.assertIn(data.Details.tippingPoint, self.browser.page_source)
 
     def testExpiryDateExists(self):
-        self.assertIn('Expiry date:', self.browser.page_source)
+        self.assertIn(data.Details.expiryDate, self.browser.page_source)
+
+    def testDescriptionExists(self):
+        elem = self.browser.find_element_by_xpath(data.Details.descriptionElement)
+        self.assertTrue(elem)
 
     def testLogout(self):
         helper.logout(self)
         response = self.browser.current_url
-        self.assertTrue(response == 'http://localhost:8000/wecar/')
+        self.assertTrue(response == data.Details.indexURL)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
