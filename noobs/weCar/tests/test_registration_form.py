09371edfb4e1cc7b87245a4b9a4917cf31b3c79e
Ammar Arjomand <persiancookbook@gmail.com>
2018-11-01 14:52:14 +0000

    register form tests 25% done

--- noobs/weCar/tests/test_registration_form.py
+++ noobs/weCar/tests/test_registration_form.py
@@ -14,11 +14,30 @@ class Details(unittest.TestCase):
     def tearDown(self):
         self.addCleanup(self.browser.quit)
 
-    def testPageLoaded(self):
+    def testPageStatusCode(self):
         request = get(data.Register.registerURL)
         self.assertEqual(request.status_code, 200)
 
+    def testPageLoaded(self):
+        response = self.browser.current_url
+        print("=============="+response)
+        self.assertTrue(response == data.Register.registerURL)
 
+
+class DetailsNoSetUp(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.addCleanup(self.browser.quit)
+
+    def testLoadPageWhileLoggedIn(self):
+        self.browser.get(data.Register.loginURL)
+        helper.login(self)
+        self.browser.get(data.Register.registerURL)
+        response = self.browser.current_url
+        self.assertFalse(response == data.Register.registerURL)
 
 
 if __name__ == '__main__':
